fbdbc6a70a8ef3121a07a5a3fc3df4ae3cc1fd55
54niyu <54niyu@sina.com>
2015-09-05 20:27:02 +0800

    2015.9.5

---
+++ Leetcode/maximalSquare.cpp
@@ -0,0 +1,18 @@
+#include"leetcode.h"
+
+class Solution{
+public:
+	int maximalSquare(vector<vector<char>>& matrix) {
+	}
+};
+int main(){
+	vector<char> s1 = { '1', '0', '1', '0', '0', };
+	vector<char> s2 = { '1', '0', '1', '1', '1', };
+	vector<char> s3 = { '1', '0', '1', '1', '1', };
+	vector<char> s4 = { '1', '1', '1', '1', '0', };
+	vector<char> s5 = { '1', '0', '1', '0', '0', };
+	vector<vector<char>> s = {s1,s2,s3,s4,s5};
+
+
+	return 0;
+}
