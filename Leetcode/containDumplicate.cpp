de3248203538ff3851458ff1857ce91b75602067
54niyu <54niyu@sina.com>
2015-09-21 12:22:33 +0800

    Update containDumplicate.cpp

--- Leetcode/containDumplicate.cpp
+++ Leetcode/containDumplicate.cpp
@@ -1,8 +1,21 @@
-#include"leetcode.h"
-
 class Solution{
 public:
 	bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
-
+        if(k==0||nums.size()<2) return false;
+        long long low=0;
+        set<long long> windows;
+        for(int i=0;i<nums.size();i++){
+            if(i-low>k){
+                windows.erase(nums[low]);
+                low++;
+            }
+            
+            auto it=windows.lower_bound((long long)nums[i]-(long long)t);
+            if(it!=windows.end()&&abs((long long)nums[i]-*it)<=(long long)t){
+                return true;
+            }
+            windows.insert(nums[i]);
+        }
+        return false;
 	}
 };
