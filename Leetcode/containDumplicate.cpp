36c888d47f2e637d5d07c24d6289ea2b992f6616
54niyu <54niyu@sina.com>
2015-09-21 12:23:37 +0800

    2019.9.21

--- Leetcode/containDumplicate.cpp
+++ Leetcode/containDumplicate.cpp
@@ -3,6 +3,12 @@
 class Solution{
 public:
 	bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
-
+		for (int i = 0; i < nums.size(); i++){
+			for (int j = 1; j <= k && (i + j) < nums.size(); j++){
+				if (abs(nums[i + j] - nums[i]) < t)
+					return true;
+			}
+		}
+		return false;
 	}
 };
