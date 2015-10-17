4d9f8eaadef7049bacc8cfee00d749060c7d0829
54niyu <54niyu@sina.com>
2015-10-17 12:18:56 +0800

    2015.10.17

--- Leetcode/surroundedRegions.cpp
+++ Leetcode/surroundedRegions.cpp
@@ -1,63 +1,82 @@
 #include"leetcode.h"
+typedef pair<int, int> Point;
 class Solution {
 public:
 	void solve(vector<vector<char>>& board) {
-		vector<vector<int>> dp(board.size(), vector<int>(board[0].size(), 0));
-		dp[0][0] = (board[0][0] == 'O' ? 1 : 0);
-		for (int i = 1; i < board.size(); i++){
-			dp[i][0] = (board[i][0] == 'O' ? dp[i - 1][0] + 1 : 0);
+		
+		for(int i=0; i < board.size(); i++){
+			if (board[i][0] == 'O')
+				bfs(board, i, 0);
+			if (board[i][board[i].size() - 1] == 'O')
+				bfs(board, i, board[i].size() - 1);
 		}
-		for (int j = 1; j < board.size(); j++){
-			dp[0][j] = (board[0][j] == 'O' ? dp[0][j-1] + 1 : 0);
+		for (int i = 0; i < board[0].size(); i++){
+			if (board[0][i] == 'O')
+				bfs(board, 0, i);
+			if (board[board.size()-1][i] == 'O')
+				bfs(board, board.size()-1, i);
 		}
-		for (int i = 1; i < board.size(); i++){
-			for (int j = 1; j < board[i].size(); j++){
-				if (board[i][j] == 'O')
-					dp[i][j] = minzero(dp[i - 1][j], dp[i][j - 1]) + 1;
-				else
-					dp[i][j] = 0;
-			}
-		}
+
 		for (int i = 0; i < board.size(); i++){
 			for (int j = 0; j < board[i].size(); j++){
-				cout << dp[i][j] << " ";
-			}
-			cout << "\n";
-		}
-		return;
-	}
-	int minzero(int x, int y){
-		if (x == 0 && y == 0)
-			return 0;
-		else {
-			if (x != 0 && y != 0){
-				return (x <y ? x : y);
-			}
-			else if (x == 0){
-				return y;
-			}
-			else {
-				return x;
+				if (board[i][j] == 'C')
+					board[i][j] = 'O';
+				else
+					board[i][j] = 'X';
 			}
 		}
 	}
+	void bfs(vector<vector<char>> &board,int i,int j){
+		queue<Point> Q;
+		Q.push(Point(i, j));
+
+		while (!Q.empty()){
+			Point temp = Q.front();
+			board[temp.first][temp.second] = 'C';
+			Q.pop();
+			if (temp.first>0&&board[temp.first - 1][temp.second] == 'O'){
+				Q.push(Point(temp.first - 1, temp.second  ));
+			}
+			if (temp.first<board.size()-1&&board[temp.first + 1][temp.second  ] == 'O'){
+				Q.push(Point(temp.first + 1, temp.second  ));
+			}
+			if (temp.second<board[0].size()-1&&board[temp.first  ][temp.second + 1] == 'O'){
+				Q.push(Point(temp.first  , temp.second + 1));
+			}
+			if (temp.second>0&&board[temp.first ][temp.second - 1] == 'O'){
+				Q.push(Point(temp.first  , temp.second - 1));
+			}
+		}
+	}
+
 };
 int main(){
-	int side = 10;
-	vector<vector<char>> board(side, vector<char>(side));
+	int side = 14;
+	vector<vector<char>> board(side, vector<char>(side,'O'));
 	for (int i = 0; i < side; i++){
 		for (int j = 0; j < side; j++){
 			int a = rand() % 2;
-			if (a == 1 )
-				board[i][j]='X';
+			if (a == 1)
+				board[i][j] = 'X';
 			else
-				board[i][j]='O';
+				board[i][j] = 'O';
 			cout << board[i][j] << " ";
 		}
-		cout << endl;
+		cout << "\n";
 	}
-
 	Solution s;
 	s.solve(board);
+	cout << "---after modifiled\n";
+	for (int i = 0; i < side; i++){
+		for (int j = 0; j < side; j++){
+//			int a = rand() % 2;
+//			if (a == 1)
+//				board[i][j] = 'X';
+//			else
+//				board[i][j] = 'O';
+			cout << board[i][j] << " ";
+		}
+		cout << "\n";
+	}
 	return 0; 
 }
