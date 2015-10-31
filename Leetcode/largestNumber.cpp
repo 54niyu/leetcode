2c854cbfe201578a99749625b58f042977bfef23
54niyu <54niyu@sina.com>
2015-10-31 19:26:31 +0800

    2015.10.31

--- Leetcode/largestNumber.cpp
+++ Leetcode/largestNumber.cpp
@@ -1,62 +1,38 @@
 #include"leetcode.h"
 
-//class Solution {
-//public:
-//	string largestNumber(vector<int>& nums) {
-//		stringstream st;
-//		vector<string> snums;
-//		string s;
-//		for (auto n : nums){
-//			st << n;
-//			st >> s;
-//			st.clear();
-//			snums.push_back(s);
-//		}
-//		sort(snums.begin(), snums.end(), compare);
-//
-//		if (snums[0] == "0")
-//			return "0";
-//	
-//		string res = "";
-//		for (auto item : snums)
-//			res += item;
-//
-//		return res;
-//	}
-//	bool compare(string a, string b){
-//		return (a + b) > (b + a);
-//	}
-//};
-//int main(){
-//	Solution s;
-//	vector<int> temp;
-//	for (int i = 999; i >=100; i--){
-//		temp.push_back(i);
-//	}
-//	s.largestNumber(temp);
-//	return 0;
-//}
-
 class Solution {
 public:
-	int maxSubArray(vector<int>& nums) {
-		if (nums.size() == 1)
-			return nums[0];
-		vector<vector<int>> dp(nums.size(),nums);
-		int max = nums[0];
-		for (int i = 1; i < nums.size(); i++){
-			for (int j = 0; j + i < dp[i].size(); j++){
-				dp[i][j]  =dp[i-1][j]+nums[j + i];
-				if (dp[i][j] > max)
-					max = dp[i][j];
-			}
+	string largestNumber(vector<int>& nums) {
+		stringstream st;
+		vector<string> snums;
+		string s;
+		for (auto n : nums){
+			st << n;
+			st >> s;
+			st.clear();
+			snums.push_back(s);
 		}
-		return max;
+		sort(snums.begin(), snums.end(), compare);
+
+		if (snums[0] == "0")
+			return "0";
+	
+		string res = "";
+		for (auto item : snums)
+			res += item;
+
+		return res;
+	}
+	static bool compare(string a, string b){
+		return (a + b) > (b + a);
 	}
 };
 int main(){
 	Solution s;
-	vector<int> temp = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
-	cout<<s.maxSubArray(temp);
+	vector<int> temp;
+	for (int i = 999; i >=100; i--){
+		temp.push_back(i);
+	}
+	s.largestNumber(temp);
 	return 0;
 }
--- Leetcode/maximumGap.cpp
+++ Leetcode/maximumGap.cpp
@@ -4,7 +4,6 @@ public:
 	int maximumGap(vector<int>& nums) {
 		if (nums.size()<2)
 			return 0;
-		//		int where = -1;
 		mysort(nums, 0, nums.size() - 1, 30);
 		int max = -1;
 		for (int i = 1; i<nums.size(); i++){
@@ -23,14 +22,12 @@ public:
 				while (j > i&&digit(nums[j], k) == 1) j--;
 				swap(nums[i], nums[j]);
 			}
-			//i move to end but not determinate end is 0 or 1
 
 			//all 0 or 1
 			if (digit(nums[end], k) == 0 || digit(nums[start], k) == 1){
 				mysort(nums, start, end, k - 1);
 			}
 			else{
-
 				mysort(nums, start, j - 1, k - 1);
 				mysort(nums, j, end, k - 1);
 			}
@@ -43,13 +40,3 @@ public:
 			return 0;
 	}
 };
-//int main(){
-//	vector<int> test= { 7,6,5,4,3,2,1,0 };
-//	for (int i = 0; i < 20; i++){
-//		test.push_back(rand() % 12321);
-//	}
-//	Solution s;
-//	s.maximumGap(test);
-//	cout << "here";
-//	return 0;
-//}
